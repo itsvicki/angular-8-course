d103b48cec574ac3a41a6b79f48251403b0e169b
Vicki Whiffing <vicki.whiffing@lc-ltm038.home>
2019-07-26 17:24:59 +0100

    Completion of assignment

--- assignment-3/src/app/app.component.ts
+++ assignment-3/src/app/app.component.ts
@@ -6,4 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  toggleDetails = false;
+  clickLogs = [];
+
+  onToggleDetails() {
+    this.toggleDetails = (this.toggleDetails === false);
+    this.clickLogs.push(new Date());
+  }
+
+  // Change colour on the 5th item, taking into account logIndex will be 1 behind as starts from 0, not 1
+  getTextBackgroundColor(logIndex) {
+    return (logIndex >= 4) ? 'blue' : '';
+  }
+
+  // Add class on the 5th item, taking into account logIndex will be 1 behind as starts from 0, not 1
+  getTextClass(logIndex) {
+    return (logIndex >= 4);
+  }
 }
