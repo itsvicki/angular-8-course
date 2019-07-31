6b2f104059d9ce3853e160b63a680f60860ef70c
Vicki Whiffing <vicki.whiffing@LC-LTM038.local>
2019-07-31 16:11:42 +0200

    Completed assigment

--- assignment-4/src/app/app.component.ts
+++ assignment-4/src/app/app.component.ts
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  oddComponents = [];
+  evenComponents = [];
+
+  onGameStarted(envData: {incrementingNumber: number}) {
+    if (envData.incrementingNumber % 2 === 0) {
+      this.evenComponents.push({
+        number: envData.incrementingNumber
+      });
+    }
+    else {
+      this.oddComponents.push({
+        number: envData.incrementingNumber
+      });
+    }
+  }
 }
---
+++ assignment-4/src/app/game-control/game-control.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+  @Output() gameStarted = new EventEmitter<{incrementingNumber: number}>();
+  intervalRef;
+  incrementingNumber = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onStartGame() {
+    // Every second emit a ongoing incrementing number
+    this.intervalRef = setInterval(() => { 
+      this.gameStarted.emit({
+        incrementingNumber: this.incrementingNumber++
+      });
+    }, 1000);
+  }
+
+  onStopGame() {
+    // Clear emitting event via clearing the interval
+    clearInterval(this.intervalRef);
+  }
+}
