85c76afc72bb46c9e8ac1a50cb418355156162f2
Vicki Whiffing <vicki.whiffing@LC-LTM038.local>
2019-08-01 18:51:33 +0200

    Progress so far

--- section-9-services-and-dependency-injection/src/app/new-account/new-account.component.ts
+++ section-9-services-and-dependency-injection/src/app/new-account/new-account.component.ts
@@ -12,7 +12,11 @@ import { AccountsService } from '../accounts.service';
 export class NewAccountComponent {
 
   constructor(private loggingService: LoggingService,
-              private accountService: AccountsService) { }
+              private accountService: AccountsService) { 
+    this.accountService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus);
