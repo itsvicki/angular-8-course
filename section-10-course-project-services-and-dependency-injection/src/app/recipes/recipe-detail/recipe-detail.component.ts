51513cfd67c11c43bb9fa78755f1f9c3168740d1
Vicki Whiffing <vicki.whiffing@LC-LTM038.lan>
2019-08-02 15:49:29 +0200

    Initial implementation of add to shopping list

--- section-10-course-project-services-and-dependency-injection/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ section-10-course-project-services-and-dependency-injection/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,9 +12,18 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
+  onAddToShoppingList() {
+    for (let ingredient of this.recipe.ingredients) {
+      let ingName = ingredient.name;
+      let ingAmount = ingredient.amount;
+      let newIngredient = new Ingredient(ingName, ingAmount);
+
+      this.shoppingListService.addIngredient(newIngredient);
+    }
+  }
 }
