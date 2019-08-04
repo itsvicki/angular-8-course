c3363e65a067ef3397018cee5d0d007f6e8c1142
Vicki Whiffing <vicki.whiffing@LC-LTM038.lan>
2019-08-04 21:25:00 +0200

    Created own observable and added an example error

--- section-13-understanding-observables/src/app/home/home.component.ts
+++ section-13-understanding-observables/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -13,8 +13,25 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     // interval is giving you an observable so you can subscribe
-    this.firstObsSubscription = interval(1000).subscribe(count => {
-      console.log(count);
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+
+    const customIntervalObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count); // this could also be observer.error/complete
+        
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3'));
+        }
+        
+        count++;
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customIntervalObservable.subscribe((data) => {
+      console.log(data);
     });
   }
 
