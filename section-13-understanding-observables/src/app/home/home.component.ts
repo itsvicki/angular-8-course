d3f31f149a57ab0910fa61f2d0ddc502c9245ee5
Vicki Whiffing <vicki.whiffing@LC-LTM038.local>
2019-08-05 12:41:23 +0200

    Implementing Subject

--- section-13-understanding-observables/src/app/home/home.component.ts
+++ section-13-understanding-observables/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -20,18 +21,32 @@ export class HomeComponent implements OnInit, OnDestroy {
     const customIntervalObservable = Observable.create(observer => {
       let count = 0;
       setInterval(() => {
-        observer.next(count); // this could also be observer.error/complete
+        observer.next(count);
         
+        if (count === 2) {
+          observer.complete();
+        }
+
         if (count > 3) {
           observer.error(new Error('Count is greater than 3'));
         }
         
         count++;
       }, 1000);
-    });
+    }); 
 
-    this.firstObsSubscription = customIntervalObservable.subscribe((data) => {
+    this.firstObsSubscription = customIntervalObservable
+    .pipe(filter((data: number) => {
+      return data > 0;
+    }), map((data: number) => {
+      return 'Round: ' + (data + 1);
+    }))
+    .subscribe((data) => {
       console.log(data);
+    }, error => {
+      console.log(error);
+    }, () => {
+      console.log('Completed!');
     });
   }
 
